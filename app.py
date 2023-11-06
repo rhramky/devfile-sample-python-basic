99cdc8de6db24d77b4c56fd2f6652716632dd8ba
rhramky <54615273+rhramky@users.noreply.github.com>
2023-11-06 17:43:04 +0530

    Update app.py
    Changed the return function

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello DevNation India!"
+    return "Hello DevNation Mandya!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
